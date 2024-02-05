4af9edfb4147f83063cf9a269d002cd19e13fd15
Brox0rz <brox0rz@gmail.com>
2024-02-04 22:42:52 -0800

    add attribution

--- week_4/hemsouvanh-assignment4.2.js
+++ week_4/hemsouvanh-assignment4.2.js
@@ -1,4 +1,11 @@
-// This query displays all documents in the users collection
+/**
+	Title: hemsouvanh-assignment4.2.js
+    Author: Brock Hemsouvanh
+    Date: 04 Feb 2024
+    Description: MongoDB Shell commands to view the users collection from users.js.
+ */
+
+    // This query displays all documents in the users collection
 db.users.find().pretty()
 
 // This query finds the user with the email address jbach@me.com
